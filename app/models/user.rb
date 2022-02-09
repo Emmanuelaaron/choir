aa832bb2f03399e2b95b6ac7a65bf416850706e3
Emmanuelaaron <emmanuelisabirye9@gmail.com>
2022-02-09 10:21:56 +0300

    adds validation to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :email, presence: true, uniqueness: true,
+                    format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Invalid email' }
+  validates :username, :password_digest, presence: true
 end
