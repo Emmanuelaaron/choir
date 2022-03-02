87c85c433782fcd185ab576d2fef7221e0270942
Emmanuelaaron <emmanuelisabirye9@gmail.com>
2022-03-02 10:49:48 +0300

    updates controllers

--- app/controllers/choirmembers_controller.rb
+++ app/controllers/choirmembers_controller.rb
@@ -1,5 +1,5 @@
 class ChoirmembersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: %i[new create]
   def new; end
 
   def welcome
